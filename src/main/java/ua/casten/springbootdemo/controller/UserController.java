79f153c02043bc225f07170a3824681a7243570b
Oleksandr Borovyk <maingroon@gmail.com>
2021-02-28 20:55:16 +0200

    Added createUser to UserController and fixed bugs

--- src/main/java/ua/casten/springbootdemo/controller/UserController.java
+++ src/main/java/ua/casten/springbootdemo/controller/UserController.java
@@ -1,8 +1,10 @@
 package ua.casten.springbootdemo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import ua.casten.springbootdemo.model.User;
 import ua.casten.springbootdemo.service.UserService;
 
@@ -11,12 +13,28 @@ import java.util.List;
 @Controller
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/users")
     public String findAll(Model model) {
         List<User> users = userService.findAll();
         model.addAttribute("users", users);
-        return "";
+        return "users";
+    }
+
+    @GetMapping("/user-create")
+    public String createUserForm(User user) {
+        return "user-create";
+    }
+
+    @PostMapping
+    public String createUser(User user) {
+        userService.saveUser(user);
+        return "redirect:/create-user";
     }
 }
