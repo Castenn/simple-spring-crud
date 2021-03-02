a18a550cec247b1746f3d84cfb855643d51d685a
Oleksandr Borovyk <maingroon@gmail.com>
2021-03-02 07:53:28 +0200

    Fixed git commit

---
+++ src/test/java/ua/casten/springbootdemo/SpringbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.casten.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
